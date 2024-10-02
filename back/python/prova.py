80010ac26340f28ffa3634f5ca23d921648a0201
Eric Gómez Vilà <105845066+EGVPedralbes@users.noreply.github.com>
2024-10-02 12:57:03 +0200

    Prova python

---
+++ back/python/prova.py
@@ -0,0 +1,12 @@
+import json
+route='../db/dades.json'
+with open(route) as file:
+    data = json.load(file)
+for pregunta in data['preguntes']:
+    print(pregunta['pregunta'])
+    for resposta in pregunta['respostes']:
+        if (resposta==pregunta['respostes'][pregunta['resposta_correcta']]):
+            print(resposta,'correcta')
+        else:
+            print(resposta)
+        
